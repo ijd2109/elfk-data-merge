afd60b7cb31b251ba5fca2074568df07214dc68e
ijd2109 <43050076+ijd2109@users.noreply.github.com>
2019-09-05 14:53:54 -0400

    data engineering (tidying) scripts
    Most require the 'functions.R' or 'functions update.R' to be run in the environment first.

---
+++ Tidying/pq merge indexing from old script.R
@@ -0,0 +1,116 @@
+index.5 <- c(
+16,
+17,
+19,
+22,
+25,
+27,
+28,
+30,
+
+31,
+
+32,
+36,
+37,
+40,
+41,
+42,
+44,
+45,
+46,
+47,
+
+48)
+
+index.7 <-c(
+16,
+19,
+
+22,
+23,
+
+25,
+26,
+ 
+27,
+
+28,
+29,
+
+30,
+
+31,
+
+32,
+34,
+35,
+
+36,
+
+37,
+
+40,
+
+41,
+
+42,
+
+44,
+
+45,
+
+46,
+
+47,
+
+48)
+
+index.6 <- c(
+  41,
+  42,
+  43,
+  44,
+  45,
+  46,
+  47,
+  48
+)
+
+index.13<- c(
+  1, 
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9,
+  10,
+  11,
+  13,
+  13,
+  14,
+  15,
+  16,
+  17,
+  18,
+  19,
+  20,
+  21
+)
+
+index.14 <- c(
+  Parent_Q.3 [3, 14] = "Just as it relates to depression and anxiety Self-harm  suicidal ideation impulsive reactions to coping rather than thoughtful"
+  Parent_Q.3 [5, 14] = "ADHD and depression"
+  Parent_Q.3 [6, 14] = "Some concern about handling anxiety + fear"
+  Parent_Q.3 [8, 14] = "Impulse control and anger issues"
+  Parent_Q.3 [10, 14] = "I wish she were more comfortable making friends."
+  Parent_Q.3 [14, 14] = "ADHD Extreme emotional responses when hungry. Always feeling hungry. Oppositional behaviors"
+  Parent_Q.3 [15, 14] = "Anxiety and Depression"
+  Parent_Q.3 [16, 14] = "ADHD behavioral issues"
+  Parent_Q.3 [17, 14] = "she needs to learn to regulate her emotions better, stop complaining about nothing and learn to be more flexible."
+  Parent_Q.3 [18, 14] = "anxious attachment"
+  Parent_Q.3 [20, 14]
+)
